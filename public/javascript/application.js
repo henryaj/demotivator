587ec93fa494faf23984970e35eb7106d80f15d8
Henry Stanley <henry@henrystanley.com>
2014-08-04 12:27:35 +0100

    Added more functionality, including image select and captions

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,3 +1,17 @@
+// 1. Takes text input from the search box and runs a google image search.
 $(document).on('click', '#go-search', function() {
 	findImagesOnGoogle({keywords: $('#search-term').val(), container: '#search-results'})
+});
+
+//2. When user clicks an image, loads it into the workspace.
+$(document).on('click', '#search-results img', function() {
+	var url = $(this).data('url');
+	$("#workspace img").remove();
+	var img = $("<img>").attr('src', url);
+	$("#workspace").append(img);
+});
+
+//3. Takes the caption the user types and puts it into #caption
+$(document).on('input', '#text', function () {
+	$("#caption").text($(this).val());
 });
